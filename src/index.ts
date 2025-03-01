48ffbb045c8d94bffd68b8b3880befbdd6f31e97
Shengming Yuan <shengming@usf.edu>
2025-02-28 21:01:55 -0500

    add cookie secure

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ const sessionSetting: session.SessionOptions = {
 if (app.get('env') === 'production') {
   app.set('trust proxy', 1);
   sessionSetting.cookie = {
-    httpOnly: true,
+    secure: true,
     sameSite: 'none',
   }
 }
